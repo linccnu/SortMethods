342b60a31f6ed652b5a20cda18185aa30166a54f
root <lin_nudt0625@163.com>
2018-03-17 23:30:21 +0800

    remove config.sh file, but touch makeAll.sh bash file to make it easy to compiler and excute

---
+++ makeAll.sh
@@ -0,0 +1,76 @@
+#########################################################################
+# Author: 0054
+# Created Time: 2018年03月13日 星期二 20时53分04秒
+# File Name: makeAll.sh
+# Description:create build&bin file folder and compiler the source codes
+#########################################################################
+#!/bin/bash
+
+cur_dir=$(pwd)
+
+bin_file="$cur_dir/bin"
+build_file="$cur_dir/build"
+
+#cmake and make executable file, excute it!
+ProcDemo()
+{
+	if [ ! -d "$bin_file" ];then
+		echo "---bin folder is not exit in dir:$cur_dir, create it!"
+		mkdir -p $bin_file
+		#chmod 777 $bin_file
+	fi
+
+	if [ ! -d "$build_file" ];then
+		echo "---build folder is not exit in dir:$cur_dir, create it!"
+		mkdir -p $build_file
+		#chmod 777 $build_file
+	fi
+
+	#begin to build the makefile and executable file
+	echo ""
+	cd $build_file
+	cmake ..
+	make
+	echo ""
+	echo "---compiler source codes and make executable file ok!"
+	echo ""
+	
+	#begin to excute the executable file
+	cd $bin_file
+	./demo
+	echo ""
+}
+
+
+#clear the invalid files or folders
+Clear()
+{
+	
+	if [ -d "$bin_file" ];then
+		rm -rf $bin_file
+		echo "---delete folder:bin ok."
+	fi
+
+	if [ -d "$build_file" ];then
+		rm -rf $build_file
+		echo "---delete folder:build ok."
+	fi
+}
+
+
+#echo "************begin to excute**********"
+echo "*************选择执行类型**************"
+echo "*********0---生成可执行文件并执行******"
+echo "*********1---清空无效的文件夹**********"
+
+read -p "请选择操作类型:" type
+case $type in
+	0)
+		echo "---select to compiler executable file and excute."
+		ProcDemo
+		;;
+	1)
+		echo "---select to remove the invalid folders."
+		Clear
+		;;
+esac
